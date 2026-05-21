6d2d47b2868b9ca767a65d6929abc6f278f7f99a
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-21 10:12:55 +0200

    chore: Update ESLint configuration to disable specific React rules for better developer flexibility

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,11 @@ export function eslintConfig(options: ESLintConfigOptions = {}, ...restParams: R
     },
 
     rules: {
+      // Devs should know what they are doing
+      'react/exhaustive-deps': 'off',
+      'react/set-state-in-effect': 'off',
+
+      // Style
       'style/brace-style': ['error', '1tbs'],
       'style/arrow-parens': ['error', 'always'],
       'style/no-multiple-empty-lines': [
