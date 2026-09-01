4f78c1b807c0a880109103a94aab3c6f13ae37b8
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-09-01 11:07:46 +0200

    refactor: Simplify ESLint configuration structure and enhance tailwindcss support in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,40 +5,50 @@ type ESLintConfigOptions = Parameters<typeof antfu>[0] & { tailwindcss?: boolean
 type RestParams = Parameters<typeof antfu> extends [any?, ...infer T] ? T : never;
 
 export function eslintConfig(options: ESLintConfigOptions = {}, ...restParams: RestParams) {
-  return antfu({
-    ...options,
+  return antfu(
+    {
+      ...options,
 
-    stylistic: {
-      indent: 2,
-      semi: true,
-      quotes: 'single',
-      ...((options?.stylistic ?? {}) as object),
-    },
+      stylistic: {
+        indent: 2,
+        semi: true,
+        quotes: 'single',
+        ...((options?.stylistic ?? {}) as object),
+      },
 
-    rules: {
+      rules: {
       // Devs should know what they are doing
-      'react/exhaustive-deps': 'off',
-      'react/set-state-in-effect': 'off',
+        'react/exhaustive-deps': 'off',
+        'react/set-state-in-effect': 'off',
 
-      // Style
-      'style/brace-style': ['error', '1tbs'],
-      'style/arrow-parens': ['error', 'always'],
-      'style/no-multiple-empty-lines': [
-        'error',
-        {
-          max: 1,
-          maxBOF: 0,
-          maxEOF: 0,
-        },
-      ],
-      ...(options?.rules ?? {}),
-    },
+        // Style
+        'style/brace-style': ['error', '1tbs'],
+        'style/arrow-parens': ['error', 'always'],
+        'style/no-multiple-empty-lines': [
+          'error',
+          {
+            max: 1,
+            maxBOF: 0,
+            maxEOF: 0,
+          },
+        ],
+        ...(options?.rules ?? {}),
+      },
 
-    formatters: {
-      html: true,
-      markdown: 'prettier',
-      css: true,
-      ...((options?.formatters ?? {}) as object),
+      formatters: {
+        html: true,
+        markdown: 'prettier',
+        css: true,
+        ...((options?.formatters ?? {}) as object),
+      },
     },
-  }, ...(options.tailwindcss ? tailwind.configs['flat/recommended'] : []), ...restParams);
+    ...(options.tailwindcss
+      ? [
+          ...(Array.isArray(tailwind.configs.recommended)
+            ? tailwind.configs.recommended
+            : [tailwind.configs.recommended]),
+        ].flat() as RestParams
+      : []),
+    ...restParams,
+  );
 }
